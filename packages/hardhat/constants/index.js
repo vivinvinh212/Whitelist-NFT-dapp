0dde27c0d5dea1ca8ef61e6f73319f50ab740954
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-08-06 15:19:40 +0700

    deploy.js

---
+++ packages/hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xda7d3A996666B431ee50f864509d83CD521efBa9";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
